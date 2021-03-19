28d5f54d248ca5302cb635faec2f4224f060cb51
ArunRengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2021-03-19 08:51:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import os
 from keras.preprocessing.image import ImageDataGenerator,img_to_array
